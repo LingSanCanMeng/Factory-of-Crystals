612862aed38cf46b2c96935e12b4d2aeeebe7035
LingSan_CanMeng <yxp20050828@163.com>
2023-01-10 15:21:50 +0800

    update sieve.js

--- kubejs/server_scripts/sieve.js
+++ kubejs/server_scripts/sieve.js
@@ -1,8 +1,10 @@
 BlockEvents.rightClicked(event => {
     const {player, server, block, hand} = event
-    /*if (hand == 'MAIN_HAND' && event.player.mainHandItem === Item.of('minecraft:air')) {
+    if (hand == 'MAIN_HAND' && event.player.mainHandItem === 'minecraft:netherite_axe') {
         if (block.id === 'minecraft:dirt') {
             server.runCommandSilent(`loot spawn ${player.x} ${player.y} ${player.z} loot factory:test`)
+            player.addItemCooldown('minecraft:netherite_axe', 20)
+            player.damageHeldItem()
         }
-    }*/
+    }
 })
