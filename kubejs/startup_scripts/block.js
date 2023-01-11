e3c1c0457b813e7a8528d9e2a506c9fcae032fcb
LingSan_CanMeng <yxp20050828@163.com>
2023-01-11 17:32:03 +0800

    handheld sieve && mesh

---
+++ kubejs/startup_scripts/block.js
@@ -0,0 +1,16 @@
+StartupEvents.registry(`block`, e => {
+    let machine = (name, layer) => {
+        let id = name.toLowerCase()
+        e.create(`factory:` + id + `_machine`)
+            .model('kubejs:block/' + id + '_machine')
+            .material('lantern')
+            .hardness(3.0)
+            .displayName(name + ' Machine')
+            .notSolid()
+            .renderType(layer)
+    }
+
+    machine('Andesite', "solid")
+    machine('Brass', "translucent")
+    machine('Copper', "cutout")
+})
--- kubejs/startup_scripts/item.js
+++ kubejs/startup_scripts/item.js
@@ -1,16 +1,20 @@
-StartupEvents.registry(`item`, event => {
-    event.create(`factory:shard_of_shards`).displayName(`万物碎片`).texture(`factory:item/shard_of_shards`)
+StartupEvents.registry(`item`, e => {
+    e.create(`factory:shard_of_shards`).displayName(`万物碎片`).texture(`factory:item/shard_of_shards`)
 
     //hammer
-    event.create(`factory:wooden_hammer`, `pickaxe`).tier(`wood`).displayName(`木质粉碎锤`).maxDamage(64).texture(`factory:item/hammers/wooden_hammer`)
-    event.create(`factory:stone_hammer`, `pickaxe`).tier(`stone`).displayName(`石质粉碎锤`).maxDamage(128).texture(`factory:item/hammers/stone_hammer`)
-    event.create(`factory:iron_hammer`, `pickaxe`).tier(`iron`).displayName(`铁质粉碎锤`).maxDamage(256).texture(`factory:item/hammers/iron_hammer`)
-    event.create(`factory:diamond_hammer`, `pickaxe`).tier(`diamond`).displayName(`钻石质粉碎锤`).maxDamage(512).texture(`factory:item/hammers/diamond_hammer`)
-    event.create(`factory:netherite_hammer`, `pickaxe`).tier(`netherite`).displayName(`下界合金质粉碎锤`).maxDamage(1024).texture(`factory:item/hammers/netherite_hammer`)
+    e.create(`factory:wooden_hammer`, `pickaxe`).tier(`wood`).displayName(`木质粉碎锤`).maxDamage(64).texture(`factory:item/hammers/wooden_hammer`)
+    e.create(`factory:stone_hammer`, `pickaxe`).tier(`stone`).displayName(`石质粉碎锤`).maxDamage(128).texture(`factory:item/hammers/stone_hammer`)
+    e.create(`factory:iron_hammer`, `pickaxe`).tier(`iron`).displayName(`铁质粉碎锤`).maxDamage(256).texture(`factory:item/hammers/iron_hammer`)
+    e.create(`factory:diamond_hammer`, `pickaxe`).tier(`diamond`).displayName(`钻石质粉碎锤`).maxDamage(512).texture(`factory:item/hammers/diamond_hammer`)
+    e.create(`factory:netherite_hammer`, `pickaxe`).tier(`netherite`).displayName(`下界合金质粉碎锤`).maxDamage(1024).texture(`factory:item/hammers/netherite_hammer`)
 
     //leaf string
-    event.create(`leaf_string`).displayName(`叶子绳`).texture(`minecraft:item/string`).color(0, 0x3E651F)
+    e.create(`leaf_string`).displayName(`叶子绳`).texture(`minecraft:item/string`).color(0, 0x3E651F)
 
     //b bucks
-    event.create('factory:b_bucks').displayName(`1元硬币`)
+    e.create('factory:b_bucks').displayName(`1元硬币`)
+
+    //handheld sieve && mesh
+    e.create('cryptopolis:hand_sieve').displayName('手持筛子')
+    e.create('cryptopolis:mesh').displayName('筛网')
 })
